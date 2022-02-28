636bee33677eb21eaeb33d021937233bfa5f8a90
tutinaa <95225632+tutinaa@users.noreply.github.com>
2022-02-28 16:54:56 +0300

    2

--- СПИСКИ/базовые операции.py
+++ СПИСКИ/базовые операции.py
@@ -3,13 +3,25 @@ a = []
 print('Исходный список:')
 n = 10
 for i in range(n):
-    number = randrange(101) - 50 # [-50;50]
+    number = randrange(11) - 5 # [-5;5]
     a.append(number)
     print(a[i], end=' ')
 print()
 # код начинается отсюда...
+p = 1
 summa = 0
+sp = 0
+k = 0
 for i in range(n):
+    if a[i]>0:
+        sp +=a[i]
+        k +=1
     summa += a[i]
+    if a[i]!=0:
+        p *= a[i]
 
 print('Сумма всех элементов списка равна', summa)
+print('Произведение всех  не нулевых элементов списка равна', p)
+print('Сумма положительных элементов списка равна', sp)
+print('среднее арифметическое', summa/n)
+print('положительных элементов', k)
