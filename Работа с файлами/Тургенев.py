3273cfcf8017d2ec9370fc2fbfd3a560a7484ecc
tihonov.a <tihonov.a@bk.ru>
2022-03-15 15:24:03 +0300

    1

---
+++ Работа с файлами/Тургенев.py
@@ -0,0 +1,8 @@
+f = open('разделенный на строки Тургенев.txt')
+name = f.readline()
+text = f.read()
+print('Название произведения:', name)
+print('------------------------')
+print(text)
+# print(len(s))
+# print(s.count('а'))
