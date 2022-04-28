ad6aef24559c9b3dcbbc2a7912758452527fd0d8
tihonov.a <tihonov.a@bk.ru>
2022-04-28 16:41:02 +0300

    1

--- цикл For/простые числа от а до b.py
+++ цикл For/простые числа от а до b.py
@@ -1,9 +1,9 @@
-
-b=100
-c=200
+b=1000000000
+c=1000000200
 for a in range(b,c+1):
     k=0
-    for i in range(2,a//2+1):
+    # for i in range(2,a//2+1):
+    for i in range(2, int(a**0.5)+1):
         if a % i ==0:
             k+=1
             break
